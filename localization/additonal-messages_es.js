a3a6ce352fab8036934728f76df74113e4d58721
Natal Ngétal <hobbestigrou@erakis.im>
2010-05-05 22:48:32 +0200

    Create translate file for additional methods

---
+++ localization/additonal-messages_es.js
@@ -0,0 +1,9 @@
+/*
+* Translated additional messages for the jQuery validation plugin.
+* Locale: ES
+*/
+
+jQuery.extend(jQuery.validator.messages, {         
+     ipv4: "Por favor, escribe una dirección IP v4 válida.",
+     ipv6: "Por favor, escribe una dirección IP v6 válida."
+};
---
+++ localization/additonal-messages_fr.js
@@ -0,0 +1,10 @@
+/*
+* Translated additional messages for the jQuery validation plugin.
+* Locale: FR
+*/
+
+jQuery.extend(jQuery.validator.messages, {         
+     ipv4: "Veuillez entrer une adresse IP v4 valide.",
+     ipv6: "Veuillez entrer une adresse IP v6 valide.",
+     frzipcode: "Veuillez entrer un code postale valide."
+};
